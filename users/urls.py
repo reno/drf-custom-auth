1566459c36c4eed272eb652ef91ec23fb62a5e6f
reno <renan.modenese@gmail.com>
2021-07-06 18:42:33 -0300

    Incluindo urls

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,20 @@
-from django.urls import path, include
+from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 from users import views
 
 app_name = 'users'
+
 urlpatterns = [
     path('', views.UserListView.as_view(), name='list'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='detail'),
-    path('email-confirm/<uidb64>/<token>/', views.EmailConfirmView.as_view(),
-         name='email-confirm')
+    path('<int:pk>/change-password/', views.ChangePasswordView.as_view(),
+         name='change-password'),
+    path('email-confirm/', views.EmailConfirmView.as_view(),
+         name='email-confirm'),
+    path('login/', jwt_views.TokenObtainPairView().as_view(), name='login'),
+    path('login/refresh/', jwt_views.TokenRefreshView().as_view(),
+         name='token-refresh'),
+    path('password-reset/', views.PasswordReset.as_view(), name='password-reset'),
+    path('password-reset-confirm/', views.PasswordResetConfirm.as_view(), name='password-reset-confirm')
+
 ]
