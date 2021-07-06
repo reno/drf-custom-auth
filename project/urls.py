c63b6ae7ece6fc911318827e21d249cbafee1a43
reno <renan.modenese@gmail.com>
2021-07-06 18:41:32 -0300

    Excluindo rotas de login da interface HTML

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('users/', include('users.urls'))
+    path('users/', include('users.urls')),
 ]
+
