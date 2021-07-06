91500710549e61d055779b786a3e26b9ecf28106
reno <renan.modenese@gmail.com>
2021-07-06 18:42:18 -0300

    Incluindo permission

--- users/permissions.py
+++ users/permissions.py
@@ -1,10 +1,21 @@
 from rest_framework import permissions
 
 
-class UserAccessPermission(permissions.BasePermission):
+class UserAccess(permissions.BasePermission):
     """
     Object-level permission to only allow the user to access and modify its data.
     """
 
     def has_object_permission(self, request, view, obj):
         return obj.id == request.user.id
+
+
+class CreateOrAuthenticated(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method == 'GET' and request.user.is_authenticated:
+            return True
+        elif request.method == 'POST':
+            return True
+        else:
+            return False
